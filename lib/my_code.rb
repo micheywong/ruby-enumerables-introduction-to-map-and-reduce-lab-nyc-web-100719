155bcaf5e668617106c3e1398fb3467c1dd7acf4
Michelle Wong <michellekwong2@gmail.com>
2019-09-25 16:07:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
 # My Code here....
+def map_to_negativize(source_array)
+  final_array = [ ]
+  counter = 0
+  while counter < source_array.length do
+  final_array.push(source_array[counter]* -1)
+  counter += 1
+  end
+  final_array
+end
+
+def map_to_no_change(dune)
+    final_array = [ ]
+    counter = 0
+  while counter < source_array.length do
+    final_array.push(dune)
+    counter += 1
+  end
+  final_array
+end
+
+def map_to_double(numbers)
+  final_array = [ ]
+  counter = 0
+  while counter < numbers.length do
+  final_array.push(numbers[counter]* 2)
+  counter += 1
+  end
+  final_array
+end
+
+def map_to_square(numbers)
+  final_array = [ ]
+  counter = 0
+  while counter < numbers.length do
+    final_array.push(numbers[counter]** 2)
+    counter += 1
+  end
+  final_array
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  total= starting_point
+  counter = 0
+  while counter < source_array.length do
+    total += source_array[counter]
+    counter += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  counter = 0
+  while counter < source_array.length do
+    if source_array == false
+    return false 
+    counter += 1
+    end
+  end
+  return true 
+end
+
+def reduce_to_any_true(source_array)
+  counter = 0
+  while counter < source_array.length do
+    if source_array == true
+    return true 
+    counter += 1
+    end
+  end
+  return false 
+end
